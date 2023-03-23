66881a5958472323adb4b4ada8f994958ce43fa5
Sarvagya2071 <67305190+Sarvagya2071@users.noreply.github.com>
2023-03-23 09:13:27 +0530

    Add hello.cpp

---
+++ 00.Basics/0.001hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Please enter your name: ";
+string name;
+    cin >> name;
+    cout << "Hello, " << name;
+    cout << "\n";
+return 0;
+}
