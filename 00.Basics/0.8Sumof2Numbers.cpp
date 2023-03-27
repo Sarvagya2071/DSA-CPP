91c567850822fa08f77b4b1bb1c840454600ebf8
Sarvagya2071 <67305190+Sarvagya2071@users.noreply.github.com>
2023-03-27 12:06:26 +0530

    Add Sum of 2 Numbers

---
+++ 00.Basics/0.8Sumof2Numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout<<"Enter Value of a:\n";
+    cin>>a;
+    cout<<"Enter Value of b:\n";
+    cin>>b;
+    c = a + b;
+    cout<<c; 
+    return 0;
+}
