7749af5db9e7eba1ede24719a6ec98d0704e3544
Sarvagya2071 <sarvagya2001@gmail.com>
2023-04-08 22:25:16 +0530

    Add Hashing Using Map

---
+++ 00.Basics/0.24HashingUsingMap.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i  = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    //precompute
+    map<long long, long long> mpp;
+    for(int i = 0; i < n; i++){
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+
+        //fetch
+        cout << mpp[number] << endl;
+    }
+    return 0;
+}
