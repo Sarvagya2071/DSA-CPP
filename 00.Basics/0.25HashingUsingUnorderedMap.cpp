815fbb46082ea14b10101ae561b63ff9d76604f2
Sarvagya2071 <sarvagya2001@gmail.com>
2023-04-10 12:15:09 +0530

    Add Hashing using unordered map

---
+++ 00.Basics/0.25HashingUsingUnorderedMap.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    //precompute
+    unordered_map<long long, long long> mpp;
+    for (int i = 0; i < n; i++){
+        mpp[arr[i]]++;
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+
+        //fetch
+        cout<<mpp[number]<<endl;
+    }
+    return 0;
+}
