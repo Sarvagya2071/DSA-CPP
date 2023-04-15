a15c5ab655b768389ed829712d444eaa9eaea48a
Sarvagya2071 <sarvagya2001@gmail.com>
2023-04-15 09:53:00 +0530

    Modified bubble sort ascending

--- 01.Sorting/0.1BubbleSortAscending.cpp
+++ 01.Sorting/0.1BubbleSortAscending.cpp
@@ -1,12 +1,45 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void bubblesortascending(vector<int> &arr, int n){
+//     bool swapstatus = false;
+//     for(int i = 1; i < n; i++){
+//         for(int j = 0; j < n-i; j++){
+//             if (arr[j] > arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//                 swapstatus = true;
+//             }
+//         }
+//         if (swapstatus == false) break;
+//     }
+// }
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     vector<int> arr;
+//     for(int i = 0; i < n; i++){
+//         int x;
+//         cin >> x;
+//         arr.push_back(x);
+//     }
+//     bubblesortascending(arr, n);
+//     for(int i = 0; i < n; i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     return 0;
+// }
+
 #include<iostream>
 #include<vector>
 using namespace std;
-void bubblesortascending(vector<int> &arr, int n){
+
+void bubblesort(int arr[], int n){
     bool swapstatus = false;
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < n-i; j++){
+    for (int i = 1; i < n; i++){
+        for (int j = 0; j < n-i; j++){
             if (arr[j] > arr[j+1]){
-                swap(arr[j],arr[j+1]);
+                swap(arr[j], arr[j+1]);
                 swapstatus = true;
             }
         }
@@ -14,18 +47,22 @@ void bubblesortascending(vector<int> &arr, int n){
     }
 }
 
+void printarray(int arr [], int n){
+    for (int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+}
+
 int main(){
     int n;
     cin>>n;
-    vector<int> arr;
+    int arr [n];
     for(int i = 0; i < n; i++){
-        int x;
-        cin >> x;
-        arr.push_back(x);
+        cin>>arr[i];
     }
-    bubblesortascending(arr, n);
-    for(int i = 0; i < n; i++){
-        cout<<arr[i]<<" ";
-    }
+    printarray(arr, n);
+    bubblesort(arr, n);
+    printarray(arr, n);
     return 0;
 }
