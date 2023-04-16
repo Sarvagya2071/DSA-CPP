10695e8d8703ce37957c36f4f944620e88e9cff0
Sarvagya2071 <sarvagya2001@gmail.com>
2023-04-16 09:16:41 +0530

    Modified for 3 digits

--- 00.Basics/0.8Sumof2Numbers.cpp
+++ 00.Basics/0.8Sumof2Numbers.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
+    int a, b, c, d;
     cout<<"Enter Value of a:\n";
     cin>>a;
     cout<<"Enter Value of b:\n";
     cin>>b;
-    c = a + b;
-    cout<<c; 
+    cout<<"Enter Value of c:\n";
+    cin>>c;
+    d = a + b + c;
+    cout<<d; 
     return 0;
 }
