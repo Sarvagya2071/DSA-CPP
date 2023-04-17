b6d1ebd8c0f9a884f57cdb96bb300852bdaf421e
Sarvagya2071 <sarvagya2001@gmail.com>
2023-04-17 18:39:08 +0530

    modify sum of digits

--- 00.Basics/0.9SumofDigits.cpp
+++ 00.Basics/0.9SumofDigits.cpp
@@ -1,20 +1,37 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int sum, a, b;
+//      cin>>a;
+//      sum = 0;
+//      while (a<0){
+//          b = a % 10;
+//          sum = sum + b;
+//          a = a/10;
+//      }
+//      while (a>0){
+//          b = a % 10;
+//          sum = sum + b;
+//          a = a/10;
+//     }
+//     cout<<sum<<endl;
+// }
+
 #include<iostream>
 using namespace std;
-
-int main()
-{
-     int sum, a, b;
-     cin>>a;
-     sum = 0;
-     while (a<0){
-         b = a % 10;
-         sum = sum + b;
-         a = a/10;
-     }
-     while (a>0){
-         b = a % 10;
-         sum = sum + b;
-         a = a/10;
+int main(){
+    float num;
+    cin>>num;
+    int sum = 0;
+    string s = to_string(num);
+    for(auto it : s){
+        if(isdigit(it)) sum += it - '0';
     }
-    cout<<sum<<endl;
+    if(num<0){
+        sum = -1*sum;
+    }
+    cout << sum << endl;
+    return 0;
 }
