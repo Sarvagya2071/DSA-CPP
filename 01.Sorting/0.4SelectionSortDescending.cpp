7344b7f8de99789eab811a73455025d6bcc7ce10
Sarvagya2071 <sarvagya2001@gmail.com>
2023-04-23 10:17:28 +0530

    Add Selection Sort Descending

---
+++ 01.Sorting/0.4SelectionSortDescending.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+	for(int i = 0; i <= n-2; i++){
+		int maximum = i;
+		for(int j = i; j <= n-1; j++){
+			if(arr[j] > arr[maximum]) maximum = j;
+		}
+		swap(arr[maximum], arr[i]);
+	}
+}
+
+void printarray(int arr[], int n){
+	for(int i = 0; i < n; i++){
+		cout<<arr[i]<<" ";
+	}
+	cout<<"\n";
+}
+
+int main() {
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i = 0; i < n; i++){
+		cin>>arr[i];
+	}
+	selection_sort(arr,n);
+	printarray(arr,n);
+    return 0;
+}
