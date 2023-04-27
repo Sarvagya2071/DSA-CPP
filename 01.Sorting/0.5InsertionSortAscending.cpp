59d20485a9fc6e5d949fe77f7c274712a0964573
Sarvagya2071 <sarvagya2001@gmail.com>
2023-04-27 12:08:51 +0530

    Add Insertion Sort Ascending

---
+++ 01.Sorting/0.5InsertionSortAscending.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        int j = i;
+        while(j > 0 && arr[j-1] > arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
+}
+
+void printarray(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    InsertionSort(arr,n);
+    printarray(arr,n);
+    return 0;
+}
