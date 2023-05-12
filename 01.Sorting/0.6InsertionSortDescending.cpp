fab5ad1a788f069ce25bcb6ac986450f49f796c6
Sarvagya2071 <sarvagya2001@gmail.com>
2023-05-12 19:16:07 +0530

    Add Space

--- 01.Sorting/0.6InsertionSortDescending.cpp
+++ 01.Sorting/0.6InsertionSortDescending.cpp
@@ -11,7 +11,7 @@ void InsertionSort(int arr[], int n){
     }
 }
 
-void printarray(int arr[], int n){
+void printarray(int arr[], int n){ 
     for(int i = 0; i < n; i++){
         cout<<arr[i]<<" ";
     }
