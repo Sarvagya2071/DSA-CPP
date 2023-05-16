27b76d6dffcbb3869996334677e975c240d27c7f
Sarvagya2071 <sarvagya2001@gmail.com>
2023-05-16 16:38:09 +0530

    Add secong largest number

---
+++ 02.Arrays/0.1Easy/0.2SecondLargestNumberinArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    int largest = arr[0];
+    int secondLargest = -1;
+    for(int i = 1; i < n; i++){
+        if(arr[i] > largest){
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] < largest && arr[i] > secondLargest){
+            secondLargest = arr[i];
+        }
+    }
+    cout<<secondLargest<<endl;
+    return 0;
+}
