45a03364bb811b67b5cce87246e2a7592e9f6e30
Sarvagya2071 <sarvagya2001@gmail.com>
2023-05-30 11:03:34 +0530

    Add Check Sorted Array

---
+++ 02.Arrays/0.1Easy/0.3CheckSortedArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int flag = 0;
+    for(int i = 0; i < n-1; i++){
+        if (arr[i] > arr[i+1]){
+            cout<<"Not Sorted"<<endl;
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0) cout<<"Sorted"<<endl;
+    return 0;
+}
