108b164e6ed625e318df0e3b33845bd2633165ab
Sarvagya2071 <sarvagya2001@gmail.com>
2023-05-31 22:40:10 +0530

    add space

--- 02.Arrays/0.1Easy/0.3CheckSortedArray.cpp
+++ 02.Arrays/0.1Easy/0.3CheckSortedArray.cpp
@@ -12,7 +12,7 @@ int main (){
     for(int i = 0; i < n-1; i++){
         if (arr[i] > arr[i+1]){
             cout<<"Not Sorted"<<endl;
-            flag = 1;
+            flag = 1; 
             break;
         }
     }
