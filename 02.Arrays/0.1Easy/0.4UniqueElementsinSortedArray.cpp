7f1f3635bdde65f84c71a413aa57f64abce31c4e
Sarvagya2071 <sarvagya2001@gmail.com>
2023-06-10 20:00:04 +0530

    add space

--- 02.Arrays/0.1Easy/0.4UniqueElementsinSortedArray.cpp
+++ 02.Arrays/0.1Easy/0.4UniqueElementsinSortedArray.cpp
@@ -9,7 +9,7 @@ int main(){
         cin>>arr[i];
     }
     int k = 0;
-    for(int i = 0; i < n - 1; i++){
+    for(int i = 0; i < n - 1; i++){ 
         if(arr[i] != arr[i+1]){
             k++;
         }
