63e9d295478bb02fed551fd0f79891a975e49491
Sarvagya2071 <sarvagya2001@gmail.com>
2023-06-12 15:07:49 +0530

    Modify Practice

--- 01.Sorting/practice.cpp
+++ 01.Sorting/practice.cpp
@@ -308,13 +308,13 @@
 // 	cout<<total<<endl;
 // 	return 0;
 // }
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    if (n % 2 == 0) cout << "Even"<<endl;
-    else cout << "Odd"<<endl;
-    return 0;
-}
+// int main(){
+//     int n;
+//     cin >> n;
+//     if (n % 2 == 0) cout << "Even"<<endl;
+//     else cout << "Odd"<<endl;
+//     return 0;
+// }
