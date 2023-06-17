1b004aeaab2a6d7fbcbf14161f409fe4fc7698db
Sarvagya2071 <sarvagya2001@gmail.com>
2023-06-17 11:02:53 +0530

    Modify Left Rotate

--- 02.Arrays/0.1Easy/0.5LeftRotateArraybyOnePlace.cpp
+++ 02.Arrays/0.1Easy/0.5LeftRotateArraybyOnePlace.cpp
@@ -9,7 +9,7 @@ int main(){
         cin>>arr[i];
     }
     int temp = arr[0];
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i < n; i++){
         arr[i-1] = arr[i];
     }
     arr[n-1] = temp;
