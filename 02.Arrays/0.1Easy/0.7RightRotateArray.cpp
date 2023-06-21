5b550e986a2928c60356fd9175ae3bcbc0c1cb46
Sarvagya2071 <sarvagya2001@gmail.com>
2023-06-21 09:48:29 +0530

    Modify right rotate

--- 02.Arrays/0.1Easy/0.7RightRotateArray.cpp
+++ 02.Arrays/0.1Easy/0.7RightRotateArray.cpp
@@ -1,26 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main(){
 
-    int n,k;
-    cin>>n>>k;
+    int n;
+    cin>>n;
+    int k;
+    cin>>k;
+    int t = n;
+    int s = k;
     int arr[n];
     for(int i = 0; i < n; i++){
         cin>>arr[i];
     }
     //temp array
     int temp[k];
-    for(int i = n-k; i < n; i++){
-        temp[i] = arr[i];
+    for(int i = 0; i < n - k; i++){
+        temp[i] = arr[t-k];
+        t++;
     }
-    //shifting
-    // for(int i = n - k; i < n; i++){
-    //     arr[n-k] = arr[i];
-    // }
-    // //put back temp
-    // for(int i = 0; i < n-k; i++){
-    //     arr[i] = temp[i];
-    // }
+    // //shifting all values to temp
+    for(int i = 0; i < n - k; i++){
+        temp[s] = arr[i];
+        s++;
+    }
+    //printing temp
     for(int i = 0; i < n; i++){
         cout<<temp[i]<<" ";
     }
