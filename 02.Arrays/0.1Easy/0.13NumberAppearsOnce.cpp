569cabf810969d9c28a9b449d2c2dee8e00af1a3
Sarvagya2071 <sarvagya2001@gmail.com>
2023-07-01 21:17:34 +0530

    add Number appear once

---
+++ 02.Arrays/0.1Easy/0.13NumberAppearsOnce.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<map>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    //better
+    // map<long long, int> mpp;
+    // for(int i = 0; i < n; i++){
+    //     mpp[arr[i]]++;
+    // }
+    // for(auto it : mpp){
+    //     if(it.second == 1){
+    //         cout<<it.first<<endl;
+    //     }
+    // }
+
+    //optimal
+    int xorr = 0;
+    for(int i = 0; i < n; i++){
+        xorr = xorr^arr[i];
+    }
+    cout<<xorr<<endl;
+
+    return 0;
+}
