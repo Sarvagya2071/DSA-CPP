7c4a1a6ca12eb4a14843890d07209c4cffb02765
Sarvagya2071 <sarvagya2001@gmail.com>
2023-07-06 19:09:07 +0530

    add count the occurenece of anagram

---
+++ 02.Arrays/0.4SlidingWindowProblems/0.1FixedSizeSlidingWindow/0.3CountOccurenceofAnagram.cpp
@@ -0,0 +1,27 @@
+//Count the Occurence of Anagram
+
+#include<iostream>
+#include<map>
+#include<list>
+using namespace std;
+
+int main(){
+    string txt;
+    cin>>txt;
+    string pat;
+    cin>>pat;
+    int k = pat.length();
+    map<char, int> mpp;
+    for(int i = 0; i < k; i++){
+        mpp[pat[i]];
+    }
+    int count = mpp.size();
+    int ans = 0;
+    int i = 0;
+    int j = 0;
+    while(j < txt.length()){
+        
+    }
+
+    return 0;
+}
