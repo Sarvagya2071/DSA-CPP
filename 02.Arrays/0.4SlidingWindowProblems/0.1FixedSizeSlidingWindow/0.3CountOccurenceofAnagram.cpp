fe47cc391425e33f12e987c46e0650c2f1d0184f
Sarvagya2071 <sarvagya2001@gmail.com>
2023-07-07 11:30:16 +0530

    Add count the occurenence of anagram

--- 02.Arrays/0.4SlidingWindowProblems/0.1FixedSizeSlidingWindow/0.3CountOccurenceofAnagram.cpp
+++ 02.Arrays/0.4SlidingWindowProblems/0.1FixedSizeSlidingWindow/0.3CountOccurenceofAnagram.cpp
@@ -10,18 +10,46 @@ int main(){
     cin>>txt;
     string pat;
     cin>>pat;
-    int k = pat.length();
-    map<char, int> mpp;
-    for(int i = 0; i < k; i++){
-        mpp[pat[i]];
-    }
-    int count = mpp.size();
-    int ans = 0;
     int i = 0;
     int j = 0;
-    while(j < txt.length()){
-        
+    int k = pat.length();
+    int ans = 0;
+    unordered_map<char,int> mp;
+    for(int i = 0; i<k; i++){
+        mp[pat[i]]++;
     }
+    int count = mp.size();
+    
+    while(j<txt.length()){
 
+        //calculation
+        if(mp.find(txt[j]) != mp.end()){
+            mp[txt[j]]--;
+            if(mp[txt[j]] == 0) count--;
+        }
+
+        //build the window
+        if(j-i+1 < k){
+            j++;
+        }
+
+        //window size achieved
+        else if(j-i+1 == k){
+            //1. find ans from calc
+            if(count == 0)  ans++;
+            
+            //emove calculation for the element that will be left out while sliding
+            if(mp.find(txt[i]) != mp.end()){
+                mp[txt[i]]++;
+                if(mp[txt[i]] == 1){
+                    count++;
+                }
+            }
+            //slide the window
+            i++;
+            j++;
+        }
+    }
+    cout<<ans<<endl;
     return 0;
 }
