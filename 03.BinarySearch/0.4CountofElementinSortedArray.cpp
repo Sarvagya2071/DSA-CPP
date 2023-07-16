33ddf96366531fe79536e7ba70fa53735a050bd8
Sarvagya2071 <sarvagya2001@gmail.com>
2023-07-16 10:39:27 +0530

    modify count of element

--- 03.BinarySearch/0.4CountofElementinSortedArray.cpp
+++ 03.BinarySearch/0.4CountofElementinSortedArray.cpp
@@ -4,9 +4,28 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    int ele;
+    cin>>ele;
     int arr[n];
     for(int i = 0; i < n; i++){
         cin>>arr[i];
     }
+
+    int start = 0;
+    int end = n-1;
+    int mid = 0;
+    int count = 0;
+    while(start <= end){
+        mid = start + (end-start)/2;
+        if(arr[mid] == ele){
+            count++;
+        }
+        else if(ele > arr[mid])
+            start = mid + 1;
+        else if(ele < arr[mid])
+            end = mid - 1;
+    }
+    if(arr[mid] != ele)
+        cout<<count<<endl;
     return 0;
 }
