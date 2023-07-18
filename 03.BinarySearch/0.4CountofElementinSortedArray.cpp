eaec4ae70cfa89bbdf4168c472ea9092aa78eeb9
Sarvagya2071 <sarvagya2001@gmail.com>
2023-07-18 10:34:36 +0530

    mod count of element

--- 03.BinarySearch/0.4CountofElementinSortedArray.cpp
+++ 03.BinarySearch/0.4CountofElementinSortedArray.cpp
@@ -1,6 +1,48 @@
 #include<iostream>
 using namespace std;
 
+int so(int arr[], int n, int ele){
+    int start = 0;
+    int end = n-1;
+    int mid = 0;
+    int last = -1;
+    while(start <= end){
+        mid = start +(end-start)/2;
+        if(mid[arr] == ele){
+            last = mid;
+            start = mid+1;
+        }
+        else if(ele < arr[mid]){
+            end = mid-1;
+        }
+        else if(ele > arr[mid]){
+            start = mid+1;
+        }
+    }
+    return last;   
+}
+
+int fo(int arr[], int n, int ele){
+    int start = 0;
+    int end = n-1;
+    int mid = 0;
+    int first = -1;
+    while(start <= end){
+        mid = start +(end-start)/2;
+        if(mid[arr] == ele){
+            first = mid;
+            end = mid-1;
+        }
+        else if(ele < arr[mid]){
+            end = mid-1;
+        }
+        else if(ele > arr[mid]){
+            start = mid+1;
+        }
+    }
+    return first;   
+}
+
 int main(){
     int n;
     cin>>n;
@@ -10,25 +52,10 @@ int main(){
     for(int i = 0; i < n; i++){
         cin>>arr[i];
     }
-
-    int start = 0;
-    int end = n-1;
-    int mid = 0;
     int count = 0;
-    int first = -1;
-    int last = -1;
-    while(start <= end){
-        mid = start + (end-start)/2;
-        if(arr[mid] == ele){
-            first = mid;
-            last = mid;
-        }
-        else if(ele > arr[mid])
-            start = mid + 1;
-        else if(ele < arr[mid])
-            end = mid - 1;
-    }
-    if(arr[mid] != ele)
-        cout<<count<<endl;
+    fo(arr, n, ele);
+    so(arr, n, ele);
+    count = so(arr, n, ele) - fo(arr, n, ele) + 1;
+    cout<<count<<endl;
     return 0;
 }
