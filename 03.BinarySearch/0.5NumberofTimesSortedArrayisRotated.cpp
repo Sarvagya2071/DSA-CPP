08b8429250519c906587d74d81d5830ae16fe8df
Sarvagya2071 <sarvagya2001@gmail.com>
2023-08-02 14:42:45 +0530

    modify Number of Times Sorted Array is Rotated

--- 03.BinarySearch/0.5NumberofTimesSortedArrayisRotated.cpp
+++ 03.BinarySearch/0.5NumberofTimesSortedArrayisRotated.cpp
@@ -11,6 +11,23 @@ int main(){
     }
     int start = 0;
     int end = n-1;
+    if(arr[start] <= arr[end]){
+        cout<<start<<endl;
+        return 0;
+    }
     
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        if(arr[start] <= arr[end]){
+            cout<<start<<endl;
+            break;
+        }
+        else if(arr[start] > arr[mid]){
+            end = mid;
+        } 
+        else if(arr[mid] > arr[end]) {
+            start = mid+1;
+        }
+    }
     return 0;
 }
