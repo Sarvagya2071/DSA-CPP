bd6b233e677fbc3865f3abff977cc059318ad8c5
Sarvagya2071 <sarvagya2001@gmail.com>
2023-08-05 13:31:33 +0530

    add square root of a number

---
+++ 03.BinarySearch/0.7SquareRootofanElement.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int start = 1;
+    int end = n;
+    int ans = 0;
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        int k = mid*mid;
+        if(k == n){
+            ans = mid;
+            cout<<mid<<endl;
+            return 0;
+        }
+        else if(k > n){
+            end = mid-1;
+        }
+        else if(k < n){
+            start = mid+1;
+        }
+    }
+    cout<<end<<endl;
+    return 0;
+}
