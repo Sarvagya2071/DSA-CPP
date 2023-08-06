f01dfa58614e1b5a2809622df80edd1ce600498d
Sarvagya2071 <sarvagya2001@gmail.com>
2023-08-06 18:36:01 +0530

    add agressive cows

---
+++ 03.BinarySearch/0.8AgressiveCows.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int cows;
+    cin>>cows;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    int start = 0;
+    return 0;
+}
