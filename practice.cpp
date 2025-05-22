af9c378ff222bc70decd193525bbb7e946b31cca
Sarvagya2071 <sarvagya2001@gmail.com>
2025-05-22 07:19:58 +0530

    add s

--- practice.cpp
+++ practice.cpp
@@ -206,12 +206,13 @@
 // Prime or not
 #include<iostream>
 using namespace std;
-    
+
 int main(){
     int n;
     cin>>n;
     int flag = 0;
 
+    
     if(n <= 1){
         cout<< "Not a prime number"<<endl;
         return 0;
